82710a7163a9029c69180c82b2d0d03d5d2a7292
Delio Rodriguez <deliolorenzo28@gmail.com>
2024-10-17 20:19:32 -0400

    Listo

---
+++ Crear las vistas para los indicadores/Productos Más Vendidos por Categoría.sql
@@ -0,0 +1,18 @@
+
+--Productos Más Vendidos por Categoría
+
+CREATE VIEW vw_ProductosMasVendidosPorCategoria AS
+SELECT 
+    C.CategoryName AS Categoria,
+    P.ProductName AS Producto,
+    SUM(OD.Quantity) AS UnidadesVendidas
+FROM 
+    [Order Details] OD
+JOIN 
+    Products P ON OD.ProductID = P.ProductID
+JOIN 
+    Categories C ON P.CategoryID = C.CategoryID
+GROUP BY 
+    C.CategoryName, P.ProductName;
+
+	select * from vw_ProductosMasVendidosPorCategoria
